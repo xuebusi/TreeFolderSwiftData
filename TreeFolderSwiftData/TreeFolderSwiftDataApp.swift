b2775862a514c676e39c816bd196c7437bbee22b
xuebusi <xuebusi@qq.com>
2024-09-28 10:16:08 +0800

    init

--- TreeFolderSwiftData/TreeFolderSwiftDataApp.swift
+++ TreeFolderSwiftData/TreeFolderSwiftDataApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct TreeFolderSwiftDataApp: App {
@@ -13,5 +14,6 @@ struct TreeFolderSwiftDataApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Directory.self)
     }
 }
